946622f1b98bd5c284acce19f37c28a27193b358
fareedawan1 <ghulam05fareed@gmail.com>
2022-09-05 18:41:11 +0500

    Add role to devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,15 @@
 class User < ApplicationRecord
+  extend Devise::Models
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  enum role: [:artist, :manager]
+  after_initialize :set_default_role, :if => :new_record?
+
+  def set_default_role
+    self.role ||= :artist
+  end
+  
 end
