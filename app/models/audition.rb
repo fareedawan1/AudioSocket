887aad4665c682ed5f7ed91b9711ed4a979d1a63
fareedawan1 <ghulam05fareed@gmail.com>
2022-09-12 19:25:05 +0500

    add status to auditions

--- app/models/audition.rb
+++ app/models/audition.rb
@@ -9,6 +9,7 @@ class Audition < ApplicationRecord
   # Enums
   enum genre: %i[Acapella Acid Jazz Acoustic Acid_Punk Alternative Hip_Hop Ambient Avantgarde Bass Blues Cabaret
                  Celtic_Chamber Chanson Chorus Christian_Rap Cinematic Classical Classic_Rock Club Comedy Country Cult]
+  enum status: %i[Pending Accepted Rejected Delete]
 
   # Validation
   validate do
